90578e164d06779c857c69eee3ab866dd8357988
Thomas Littleton <tlittleton@academic.rrc.ca>
2025-11-30 13:20:30 -0600

    two

---
+++ add_inventory_item/lambda_function.py
@@ -0,0 +1,43 @@
+import json
+import boto3
+import uuid
+
+
+def lambda_handler(event, context):
+    # Parse incoming JSON data
+    try:
+        data = json.loads(event["body"])
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps("Bad request. Please provide the data."),
+        }
+
+    # DynamoDB setup
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("Inventory")
+
+    # Generate a unique ID
+    unique_id = str(uuid.uuid4())
+
+    # Insert data into DynamoDB
+    try:
+        table.put_item(
+            Item={
+                "_id": unique_id,
+                "Name": data["Name"],
+                "Description": data["Description"],
+                "Qty": data["Qty"],
+                "Price": data["Price"],
+                "Location_id": data["Location_id"],
+            }
+        )
+        return {
+            "statusCode": 200,
+            "body": json.dumps(f"Item with ID {unique_id} added successfully."),
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"Error adding item: {str(e)}"),
+        }
